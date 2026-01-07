2b7ab87b7c6a0e45ddf4d08bc971f8f2297fb561
Hoang Dieu <dieubh234112e@st.uel.edu.vn>
2026-01-08 00:15:07 +0700

    solve : hash map - use complement methods

---
+++ 1-twoSum1.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    let map = new Map()
+    for (let i=0; i<nums.length; i++){
+        map.set(nums[i], i)
+    }
+    for (let i =0; i<nums.length; i++){ // i: giá trị 1 cần tìm 
+        let complement = target - nums[i] // complement giá trị 2 cần tìm 
+        if (map.has(complement) && (map.get(complement)!==i)){
+            return [i, map.get(complement)] // lấy value (=index) của giá trị phần bù 
+        }
+    }
+}
