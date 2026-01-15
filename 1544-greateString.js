7c45c3ed1239ca64f1042e1d020a815ed8886f37
Hoang Dieu <dieubh234112e@st.uel.edu.vn>
2026-01-15 08:26:58 +0700

    solve : stack - monotonic array (always inc or dec by 1)

--- 1544-greateString.js
+++ 1544-greateString.js
@@ -2,13 +2,14 @@
  * @param {string} s
  * @return {string}
  */
+// bad Pair of letter : là cùng 1 ký tự nhưng in hoa - in thường 
 var makeGood = function (s) {
     if (s.length === 0) return s
     let stack = []
 
     for (let i = 0; i < s.length; i++) {
         let top = stack[stack.length - 1] || ''
-        if (s[i] !== top && (s[i].toLowerCase() === top.toLowerCase())) {
+        if (s[i] !== top && (s[i].toLowerCase() === top.toLowerCase())) { // thay vì đi theo khẳng định thì thử chọn phủ định 
             stack.pop()
         } else {
             stack.push(s[i])
---
+++ 896-monotonicArr.js
@@ -0,0 +1,15 @@
+
+var isMonotonic = function(nums) {
+    let inc = true 
+    let dec = true 
+    for (let i = 0; i<nums.length; i++){
+        if (nums[i] > nums[i+1]){
+            inc = false
+        } 
+        else if (nums[i] < nums[i+1]){
+            dec = false 
+        }
+    }
+    return dec || inc 
+
+};
